623568aeceff557225e8910d8a0f414223ee6d4c
Held Grijo <von.grijo@pmm.am.gov.br>
2019-09-04 17:22:49 -0400

    Ajustes para capturar o click e armazenar o estado do tabuleiro no state compartilhado

--- src/index.js
+++ src/index.js
@@ -15,9 +15,9 @@ class Square extends React.Component {
     return (
       <button
         className="square"
-        onClick={() => this.setState({value:'X'})}
+        onClick={() => this.props.onClick()}
       >
-        {this.state.value}
+        {this.props.value}
       </button>
     );
   }
@@ -32,8 +32,17 @@ class Board extends React.Component {
     };
   }
 
+  handleClick(i){
+    const squares = this.state.squares.slice();
+    squares[i] = 'X';
+    this.setState({squares: squares});
+  }
+
   renderSquare(i) {
-    return <Square value={this.state.squares[i]}/>;
+    return <Square 
+    value={this.state.squares[i]}
+    onClick={()=>this.handleClick(i)}
+    />;
   }
 
   render() {
